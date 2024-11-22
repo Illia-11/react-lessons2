a8eb2c8be1fb6878a4394bbbb515ea791217e7c9
Illia Piddubnyi <illya2008zp@gmail.com>
2024-11-22 20:29:01 +0200

    Add key prop example

--- src/App.jsx
+++ src/App.jsx
@@ -83,10 +83,6 @@ function App() {
   // </>;
   // }
 
-  const productsComponents = products.map((product) => {
-    return <Product product={product} />;
-  });
-
   return (
     <>
       {/* <Greeting userFirstName="John" userLastName="Doe" />
