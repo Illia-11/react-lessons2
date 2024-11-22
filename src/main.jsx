fb52fb2ee94df95ae33abdcd63e18ffdd8c8f5d0
Illia Piddubnyi <illya2008zp@gmail.com>
2024-11-22 12:15:00 +0200

    Add Fragment component usage example

--- src/main.jsx
+++ src/main.jsx
@@ -52,6 +52,28 @@ function MyFunctionalComponent() {
 }
 
 // Щоб намалювати компонент його закидуємо у createElement першим параметром
-const elem = React.createElement(MyFunctionalComponent);
+const funcElem1 = React.createElement(MyFunctionalComponent);
+const funcElem2 = React.createElement(MyFunctionalComponent);
 
-createRoot(document.getElementById('root')).render(elem);
+const classElem1 = React.createElement(MyClassComponent);
+const classElem2 = React.createElement(MyClassComponent);
+
+// const app = React.createElement('div', null, classElem1, classElem2, funcElem1, funcElem2);
+
+// Фрагмент - спеціальний компонент, який дозволяє створювати обгортку для елементів, яку реакт потім не буде малювати у верстці. Пропси (2 аргумент) завжди мають бути null
+// const frag = React.createElement(React.Fragment, null, classElem1, classElem2, funcElem1, funcElem2);
+
+function App() {
+  const funcElem1 = React.createElement(MyFunctionalComponent);
+  const funcElem2 = React.createElement(MyFunctionalComponent);
+
+  const classElem1 = React.createElement(MyClassComponent);
+  const classElem2 = React.createElement(MyClassComponent);
+
+  // return React.createElement('div', null, classElem1, classElem2, funcElem1, funcElem2);
+  return React.createElement(React.Fragment, null, classElem1, classElem2, funcElem1, funcElem2);
+}
+
+const app = React.createElement(App);
+
+createRoot(document.getElementById('root')).render(app);
