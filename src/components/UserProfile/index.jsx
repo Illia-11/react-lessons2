e57beb84d4110d9a3ca6efcc96c502fbaf56caa2
Illia Piddubnyi <illya2008zp@gmail.com>
2024-11-28 18:33:37 +0200

    Add state elevation example

--- src/components/UserProfile/index.jsx
+++ src/components/UserProfile/index.jsx
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 
 class UserProfile extends Component {
-  state = {
-    user: {
-      id: 0,
-      firstName: "User",
-      lastName: "Userenko",
-      isMale: true,
-      email: "testuser@gmail.com",
-      age: 12,
-    },
-  }
 
   render() {
-    const { user } = this.state
+    const { user } = this.props;
 
-    return (
-      <article>
-        <h1>{user.firstName} {user.lastName}</h1>
-        <p>Gender: {user.isMale ? "Male" : "Female"}</p>
-        <p>Age: {user.age}</p>
-        <p>Email: {user.email}</p>
+    if(user) {
+      return (
+        <article>
+          <h1>{user.firstName} {user.lastName}</h1>
+          <p>Gender: {user.isMale ? "Male" : "Female"}</p>
+          <p>Age: {user.age}</p>
+          <p>Email: {user.email}</p>
+        </article>
+      );
+    } else {
+      return <article>
+        <h1>Увійдіть щоб переглянути свої дані</h1>
       </article>
-    );
+    }
+
   }
 }
 
