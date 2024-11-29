4ea917c55a7f536a1612b2e41eb970890d14ed92
Illia Piddubnyi <illya2008zp@gmail.com>
2024-11-29 20:01:02 +0200

    Add basic pagination

--- src/components/UsersLoader/index.jsx
+++ src/components/UsersLoader/index.jsx
@@ -5,14 +5,19 @@ class UsersLoader extends Component {
     users: [],
     error: null,
     isLoading: false,
+    currentPage: 1,
   };
 
   componentDidMount() {
+    const { currentPage } = this.state;
+
     this.setState({
       isLoading: true,
     });
 
-    fetch('https://randomuser.me/api/?seed=12345&results=10&nat=ua')
+    fetch(
+      `https://randomuser.me/api/?seed=12345&results=10&nat=ua&page=${currentPage}`
+    )
       .then((res) => res.json())
       .then(({ results: users }) => {
         // console.log(users);
@@ -32,8 +37,55 @@ class UsersLoader extends Component {
       });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.currentPage !== this.state.currentPage) {
+      const { currentPage } = this.state;
+
+      this.setState({
+        isLoading: true,
+      });
+
+      fetch(
+        `https://randomuser.me/api/?seed=12345&results=10&nat=ua&page=${currentPage}`
+      )
+        .then((res) => res.json())
+        .then(({ results: users }) => {
+          // console.log(users);
+          this.setState({
+            users,
+          });
+        })
+        .catch((error) => {
+          this.setState({
+            error,
+          });
+        })
+        .finally(() => {
+          this.setState({
+            isLoading: false,
+          });
+        });
+    }
+  }
+
+  handlePrev = () => {
+    const { currentPage } = this.state;
+
+    this.setState({
+      currentPage: currentPage > 1 ? currentPage - 1 : 1,
+    });
+  };
+
+  handleNext = () => {
+    const { currentPage } = this.state;
+
+    this.setState({
+      currentPage: currentPage + 1,
+    });
+  };
+
   render() {
-    const { users, isLoading, error } = this.state;
+    const { users, isLoading, error, currentPage } = this.state;
 
     if (isLoading) {
       return <div>LOADING ...</div>;
@@ -51,7 +103,16 @@ class UsersLoader extends Component {
       );
     });
 
-    return <div>{usersCards}</div>;
+    return (
+      <div>
+        <div>
+          <button onClick={this.handlePrev}>Prev</button>
+          <span>Поточка сторінка: {currentPage}</span>
+          <button onClick={this.handleNext}>Next</button>
+        </div>
+        {usersCards}
+      </div>
+    );
   }
 }
 
