2248da53a4ec7b2802bff77b1a0c66a6b0e65c15
Illia Piddubnyi <illya2008zp@gmail.com>
2024-12-06 18:32:58 +0200

    Add classNames libraru usage example

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { ThemeContext } from '../../context';
+import { withTheme } from '../../hocs';
+import styles from './Header.module.scss';
 import CONSTANTS from '../../configs';
-import styles from "./Header.module.scss"
+import cx from 'classnames';
 
 class Header extends Component {
   render() {
-    const { user, handleLogout, handleLogin } = this.props;
+    const { user, handleLogout, handleLogin, theme, switchTheme } = this.props;
 
     const fullName = user
       ? `${user.firstName} ${user.lastName}`.trim()
@@ -14,41 +15,51 @@ class Header extends Component {
     const logoutBtn = <button onClick={handleLogout}>Вийти</button>;
     const loginBtn = <button onClick={handleLogin}>Увійти</button>;
 
+    // let currentThemeClass;
+
+    // if (theme === CONSTANTS.THEMES.DARK_THEME) {
+    //   currentThemeClass = styles.darkTheme;
+    // } else if (theme === CONSTANTS.THEMES.LIGHT_THEME) {
+    //   currentThemeClass = styles.lightTheme;
+    // }
+
+    // const className = `${styles.container} ${currentThemeClass}`;
+
+    /*
+      функція приймає необмежену кількість аргументів
+        якщо ваш аргумент є рядком то він автоматично додається до результуючого рядка
+        якщо ваш аргумент є об'єктом то ключем вашого об'єкта має бути клас 
+          який додається до результату, а значенням - булевий вираз
+          Якщо вираз дійсний - то клас додасться, якщо ні то ні
+    */
+    const headerClasses = cx(styles.container, {
+      [styles.darkTheme]: theme === CONSTANTS.THEMES.DARK_THEME,
+      [styles.lightTheme]: theme === CONSTANTS.THEMES.LIGHT_THEME
+    });
+
     return (
-      <ThemeContext.Consumer>
-        {([theme, switchTheme]) => {
-          let currentThemeClass;
-
-          if (theme === CONSTANTS.THEMES.DARK_THEME) {
-            currentThemeClass = styles.darkTheme;
-          } else if (theme === CONSTANTS.THEMES.LIGHT_THEME) {
-            currentThemeClass = styles.lightTheme;
-          }
-
-          const className = `${styles.container} ${currentThemeClass}`;
-
-          return (
-            <header className={className}>
-              <h1>Мій сайт</h1>
-              <p>Привіт {fullName}</p>
-              {user ? logoutBtn : loginBtn}
-              <button
-                onClick={() => {
-                  const newTheme =
-                    theme === CONSTANTS.THEMES.DARK_THEME
-                      ? CONSTANTS.THEMES.LIGHT_THEME
-                      : CONSTANTS.THEMES.DARK_THEME;
-                  switchTheme(newTheme);
-                }}
-              >
-                Змінити тему на {theme === 'dark' ? 'світлу' : 'темну'}
-              </button>
-            </header>
-          );
-        }}
-      </ThemeContext.Consumer>
+      <header className={headerClasses}>
+        <h1>Мій сайт</h1>
+        <p>Привіт {fullName}</p>
+        {user ? logoutBtn : loginBtn}
+        <button
+          onClick={() => {
+            const newTheme =
+              theme === CONSTANTS.THEMES.DARK_THEME
+                ? CONSTANTS.THEMES.LIGHT_THEME
+                : CONSTANTS.THEMES.DARK_THEME;
+            switchTheme(newTheme);
+          }}
+        >
+          Змінити тему на {theme === 'dark' ? 'світлу' : 'темну'}
+        </button>
+      </header>
     );
   }
 }
 
-export default Header;
+// const HeaderWithTheme = withTheme(Header);
+
+// export default HeaderWithTheme;
+
+export default withTheme(Header);
