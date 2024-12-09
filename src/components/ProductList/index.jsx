65681e1f704ae502e559c8a565875f5910e2f095
Illia Piddubnyi <illya2008zp@gmail.com>
2024-12-09 18:45:15 +0200

    Add basic useState example

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,42 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Product from "../Product";
 
-class ProductList extends React.Component {
-  state = {
-    isReverseOrder: false,
+function ProductList({ products, toggleFavoriteProduct }) {
+  // хук стану
+  /*
+    useState - приймає початкове значення стану (не обовʼязково обʼєкт)
+    і повертає кортеж із 2 значень
+      1 елемент завжди є поточним значенням стану
+      2 елемент завжди буде персоналізованою функцією зміни стану 1 елементу
+  */
+  const [isReverseOrder, setIsReversedOrder] = useState(false);
+
+  const handleSwitchOrder = () => {
+    // функція встановлення нового стану
+    setIsReversedOrder(!isReverseOrder);
   };
 
-  handleSwitchOrder = () => {
-    this.setState({
-      isReverseOrder: !this.state.isReverseOrder,
-    });
-  };
-
-  render() {
-    const { products, toggleFavouriteProduct } = this.props;
-    const { isReverseOrder } = this.state;
-
-    const productsComponents = products.map((product) => {
-      // Взаємодія батько - дитина (пропси)
-      return (
-        <Product
-          key={product.id}
-          product={product}
-          handleSwitchOrder={this.handleSwitchOrder}
-          toggleFavouriteProduct={toggleFavouriteProduct}
-        />
-      );
-    });
-
+  const productsComponents = products.map((product) => {
+    // Взаємодія Батько - Дитина (пропси)
     return (
-      <article>
-        <h2>Products</h2>
-        <p>Sorting order: {isReverseOrder ? "reverse" : "straight"}</p>
-        <button onClick={this.handleSwitchOrder}>Switch order</button>
-        {isReverseOrder ? productsComponents.reverse() : productsComponents}
-      </article>
+      <Product
+        key={product.id}
+        product={product}
+        handleSwitchOrder={handleSwitchOrder}
+        toggleFavoriteProduct={toggleFavoriteProduct}
+      />
     );
-  }
+  });
+
+  return (
+    <article>
+      <h2>Products</h2>
+      <p>Sorting order: {isReverseOrder ? 'reverse' : 'straight'}</p>
+      <button onClick={handleSwitchOrder}>Switch order</button>
+      {isReverseOrder ? productsComponents.reverse() : productsComponents}
+    </article>
+  );
 }
 
 export default ProductList;
