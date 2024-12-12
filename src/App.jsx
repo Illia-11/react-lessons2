3fb528b613dc59a1aec547a2401dac0e5ae30859
Illia Piddubnyi <illya2008zp@gmail.com>
2024-12-12 20:45:53 +0200

    Add basic useReducer example

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { useState } from "react";
-import { UserContext, ThemeContext } from "./context";
-import CONSTANTS from "./configs";
-import Avatar from "./components/Avatar/index.jsx";
-import Header from "./components/Header/index.jsx";
-import MouseTracker from "./components/MouseTracker/index.jsx";
+import { useState } from 'react';
+import { UserContext, ThemeContext } from './context';
+import CONSTANTS from './configs';
+import Avatar from './components/Avatar';
+import Header from './components/Header';
+import MouseTracker from './components/MouseTracker';
 
 function App() {
   const [user, setUser] = useState({
     id: 0,
-    firstName: "Petro",
-    lastName: "Userenko",
+    firstName: 'Petro',
+    lastName: 'Userenko',
     isMale: true,
-    email: "testuser@gmail.com",
+    email: 'testuser@gmail.com',
     age: 12,
   });
   const [theme, setTheme] = useState(CONSTANTS.THEMES.LIGHT_THEME);
   const [isTrackerShown, setIsTrackerShown] = useState(false);
-  const [email, setEmail] = useState("");
-  const [comment, setComment] = useState("");
+  const [email, setEmail] = useState('test');
+  const [comment, setComment] = useState('');
 
   const handleLogout = () => {
     setUser(null);
@@ -26,10 +26,10 @@ function App() {
   const handleLogin = () => {
     setUser({
       id: 0,
-      firstName: "Petro",
-      lastName: "Userenko",
+      firstName: 'User',
+      lastName: 'Userenko',
       isMale: true,
-      email: "testuser@gmail.com",
+      email: 'testuser@gmail.com',
       age: 12,
     });
   };
@@ -38,33 +38,28 @@ function App() {
     setIsTrackerShown(!isTrackerShown);
   };
 
-  const changeEmail = ({ target: { value } }) => {
+  const changeEmail = ({target: {value}}) => {
     setEmail(value);
-  };
+  }
 
-  const changeComment = ({ target: { value } }) => {
+  const changeComment = ({target: {value}}) => {
     setComment(value);
-  };
+  }
 
-  const submitFrom = (e) => {
+  const submitForm =  (e) => {
     e.preventDefault();
 
-    alert("We will conect with you");
-  };
+    alert(`ми з вами зв'яжемося.`);
+  }
 
   return (
     <UserContext.Provider value={user}>
       <ThemeContext.Provider value={[theme, setTheme]}>
         <Header handleLogin={handleLogin} handleLogout={handleLogout} />
-        <Avatar src="test" alt="test" />
-        <form onSubmit={submitFrom}>
-          <input
-            type="email"
-            name="emails"
-            value={email}
-            onChange={changeEmail}
-          />
-          <textarea name="comment" value={comment} onChange={changeComment} />
+        <Avatar src='test' alt='test' />
+        <form onSubmit={submitForm}>
+          <input type='email' name='email' value={email} onChange={changeEmail} />
+          <textarea name='comment' value={comment} onChange={changeComment} />
           <button>Send comment</button>
         </form>
         <button onClick={toggleTracker}>toggle tracker</button>
