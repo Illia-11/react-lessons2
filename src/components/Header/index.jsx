dd20c096b4fb3a221f15b4877645ae5112d61e6d
Illia Piddubnyi <illya2008zp@gmail.com>
2024-12-13 18:37:29 +0200

    Add useRef example

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,33 +1,48 @@
-import { useContext } from 'react';
-import cx from 'classnames';
-import { ThemeContext, UserContext } from '../../context';
-import styles from './Header.module.scss';
-import CONSTANTS from '../../configs';
+import { useContext, useEffect, useRef } from "react";
+import cx from "classnames";
+import { ThemeContext, UserContext } from "../../context";
+import styles from "./Header.module.scss";
+import CONSTANTS from "../../configs";
 
 function Header({ dispatch }) {
   const handleLogout = () => {
-    dispatch({ type: 'logoutUser' });
+    dispatch({ type: "logoutUser" });
   };
 
   const handleLogin = () => {
     dispatch({
-      type: 'loginUser',
+      type: "loginUser",
       user: {
         id: 0,
-        firstName: 'User',
-        lastName: 'Userenko',
+        firstName: "User",
+        lastName: "Userenko",
         isMale: true,
-        email: 'testuser@gmail.com',
+        email: "testuser@gmail.com",
         age: 12,
       },
     });
   };
 
+  const inputRef = useRef(null);
+  const numberRef = useRef(0);
+
+  console.log(inputRef.current); // значення рефа
+
+  useEffect(() => {
+    console.log(inputRef.current);
+  }, [inputRef]);
+
+  useEffect(() => {
+    numberRef.current = 10; // мутація рефа - вважається побічним ефектом, не призводить до повторного рендера. Можна робити якщо треба
+    
+    // this.state.number = 5; // мутація стану - ПОМИЛКА
+  }, []);
+
   // useContext приймає об'єкт контексту та повертає значення яке у ньому знаходиться
   const [theme, switchTheme] = useContext(ThemeContext);
   const user = useContext(UserContext);
 
-  const fullName = user ? `${user.firstName} ${user.lastName}`.trim() : 'Guest';
+  const fullName = user ? `${user.firstName} ${user.lastName}`.trim() : "Guest";
 
   const logoutBtn = <button onClick={handleLogout}>Вийти</button>;
   const loginBtn = <button onClick={handleLogin}>Увійти</button>;
@@ -51,13 +66,15 @@ function Header({ dispatch }) {
               : CONSTANTS.THEMES.DARK_THEME;
 
           dispatch({
-            type: 'changeTheme',
+            type: "changeTheme",
             theme: newTheme,
           });
         }}
       >
-        Змінити тему на {theme === 'dark' ? 'світлу' : 'темну'}
+        Змінити тему на {theme === "dark" ? "світлу" : "темну"}
       </button>
+      {/* Привʼязка рефа до елементв */}
+      <input ref={inputRef} />
     </header>
   );
 }
