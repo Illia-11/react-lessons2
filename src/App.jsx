fc08b8a18a09f1f713dfdb040345b33701c405ba
Illia Piddubnyi <illya2008zp@gmail.com>
2024-12-16 19:03:40 +0200

    Add useLocation example

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
-import { Routes, Route } from 'react-router';
-import { Layout } from './pages/Layouts/Layout';
-import { AuthLayout } from './pages/AuthLayot';
-import { HomePage } from './pages/Home';
-import { AboutPage } from './pages/About';
-import { ContactsPage } from './pages/Contacts';
-import { LoginPage } from './pages/LoginPage';
-import { RegistrationPage } from './pages/RegistrationPage';
-import ProfilePage from './pages/ProfilePage';
-
+import { Routes, Route } from "react-router";
+import { Layout } from "./pages/Layouts/Layout";
+import { AuthLayout } from "./pages/AuthLayot";
+import { HomePage } from "./pages/Home";
+import { AboutPage } from "./pages/About";
+import { ContactsPage } from "./pages/Contacts";
+import { LoginPage } from "./pages/LoginPage";
+import { RegistrationPage } from "./pages/RegistrationPage";
+import ProfilePage from "./pages/ProfilePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { useEffect } from "react";
+import { useLocation } from "react-router";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
+
   return (
     <>
       {/* <Header /> */}
       <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<HomePage user={'Test User'} />} />
-          <Route path='about' element={<AboutPage />} />
-          <Route path='contacts' element={<ContactsPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage user={"Test User"} />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="contacts" element={<ContactsPage />} />
           <Route path="profiles/:profileName" element={<ProfilePage />} />
           {/* <Route path="profiles" element={<ProfileLayout />} >
             <Route index element={<UserProfile />} />
             <Route path=":profileId" element={<OtherProfile />} />
           </Route> */}
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
 
-        <Route path='/auth' element={<AuthLayout />}>
+        <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<LoginPage />} />
           <Route path="registration" element={<RegistrationPage />} />
         </Route>
